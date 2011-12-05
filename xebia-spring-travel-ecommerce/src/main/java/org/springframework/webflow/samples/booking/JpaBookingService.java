5774c952e98a8ee5965332060b352faf6a8a4c81
Pierre Laporte <pierre@pingtimeout.fr>
2011-12-05 23:43:48 +0100

    Reindented code

--- xebia-spring-travel-ecommerce/src/main/java/org/springframework/webflow/samples/booking/JpaBookingService.java
+++ xebia-spring-travel-ecommerce/src/main/java/org/springframework/webflow/samples/booking/JpaBookingService.java
@@ -1,15 +1,14 @@
 package org.springframework.webflow.samples.booking;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A JPA-based implementation of the Booking Service. Delegates to a JPA entity manager to issue data access calls
@@ -21,7 +20,7 @@ import org.springframework.util.StringUtils;
 public class JpaBookingService implements BookingService {
 
     private EntityManager em;
-	private boolean isEnabledBookings =true;
+    private boolean isEnabledBookings = true;
 
     private boolean isEnabledHotels = true;
 
@@ -30,43 +29,43 @@ public class JpaBookingService implements BookingService {
     }
 
     public void setEnabledBookings(boolean isEnabled) {
-		this.isEnabledBookings = isEnabled;
-	}
+        this.isEnabledBookings = isEnabled;
+    }
 
-	@PersistenceContext
+    @PersistenceContext
     public void setEntityManager(EntityManager em) {
-	this.em = em;
+        this.em = em;
     }
 
     @Transactional(readOnly = true)
     @SuppressWarnings("unchecked")
     public List<Booking> findBookings(String username) {
-	if (username != null) {
-			if (isEnabledBookings){
-	    return em.createQuery("select distinct b from Booking b left join fetch b.hotel f left join fetch f.bookings where b.user.username = :username order by b.checkinDate")
-		    .setParameter("username", username).getResultList();}
-		else {
-			return em.createQuery("select  b from Booking b where b.user.username = :username order by b.checkinDate")
-				    .setParameter("username", username).getResultList();
-		}
-	} else {
-	    return null;
-	}
+        if (username != null) {
+            if (isEnabledBookings) {
+                return em.createQuery("select distinct b from Booking b left join fetch b.hotel f left join fetch f.bookings where b.user.username = :username order by b.checkinDate")
+                        .setParameter("username", username).getResultList();
+            } else {
+                return em.createQuery("select  b from Booking b where b.user.username = :username order by b.checkinDate")
+                        .setParameter("username", username).getResultList();
+            }
+        } else {
+            return null;
+        }
     }
 
     @Transactional(readOnly = true)
     @SuppressWarnings("unchecked")
     public List<Hotel> findHotels(SearchCriteria criteria) {
-	    String pattern = getSearchPattern(criteria);
-        if (isEnabledHotels){
-             List<Hotel> hotels = em.createQuery("select h from Hotel h where lower(h.name) like " + pattern + " or lower(h.city) like " + pattern
-			    + " or lower(h.zip) like " + pattern + " or lower(h.address) like " + pattern).getResultList();
+        String pattern = getSearchPattern(criteria);
+        if (isEnabledHotels) {
+            List<Hotel> hotels = em.createQuery("select h from Hotel h where lower(h.name) like " + pattern + " or lower(h.city) like " + pattern
+                    + " or lower(h.zip) like " + pattern + " or lower(h.address) like " + pattern).getResultList();
             List<Hotel> res = new ArrayList<Hotel>();
             int i = 0;
-            for (Hotel h : hotels){
-                if (i>= criteria.getPage() * criteria.getPageSize()){
+            for (Hotel h : hotels) {
+                if (i >= criteria.getPage() * criteria.getPageSize()) {
                     res.add(h);
-                    if (res.size() == criteria.getPageSize()){
+                    if (res.size() == criteria.getPageSize()) {
                         break;
                     }
                 }
@@ -75,49 +74,49 @@ public class JpaBookingService implements BookingService {
             return res;
 
         } else {
-	    return em.createQuery(
-		    "select h from Hotel h where lower(h.name) like " + pattern + " or lower(h.city) like " + pattern
-			    + " or lower(h.zip) like " + pattern + " or lower(h.address) like " + pattern).setMaxResults(
-		    criteria.getPageSize()).setFirstResult(criteria.getPage() * criteria.getPageSize()).getResultList();
+            return em.createQuery(
+                    "select h from Hotel h where lower(h.name) like " + pattern + " or lower(h.city) like " + pattern
+                            + " or lower(h.zip) like " + pattern + " or lower(h.address) like " + pattern).setMaxResults(
+                    criteria.getPageSize()).setFirstResult(criteria.getPage() * criteria.getPageSize()).getResultList();
         }
     }
 
     @Transactional(readOnly = true)
     public Hotel findHotelById(Long id) {
-	return em.find(Hotel.class, id);
+        return em.find(Hotel.class, id);
     }
 
     @Transactional(readOnly = true)
     public Booking createBooking(Long hotelId, String username) {
-	Hotel hotel = em.find(Hotel.class, hotelId);
-	User user = findUser(username);
-	Booking booking = new Booking(hotel, user);
-	em.persist(booking);
-	return booking;
+        Hotel hotel = em.find(Hotel.class, hotelId);
+        User user = findUser(username);
+        Booking booking = new Booking(hotel, user);
+        em.persist(booking);
+        return booking;
     }
 
     @Transactional
     public void cancelBooking(Long id) {
-	Booking booking = em.find(Booking.class, id);
-	if (booking != null) {
-	    em.remove(booking);
-	}
+        Booking booking = em.find(Booking.class, id);
+        if (booking != null) {
+            em.remove(booking);
+        }
 
     }
 
     // helpers
 
     private String getSearchPattern(SearchCriteria criteria) {
-	if (StringUtils.hasText(criteria.getSearchString())) {
-	    return "'%" + criteria.getSearchString().toLowerCase().replace('*', '%') + "%'";
-	} else {
-	    return "'%'";
-	}
+        if (StringUtils.hasText(criteria.getSearchString())) {
+            return "'%" + criteria.getSearchString().toLowerCase().replace('*', '%') + "%'";
+        } else {
+            return "'%'";
+        }
     }
 
     private User findUser(String username) {
-	return (User) em.createQuery("select u from User u where u.username = :username").setParameter("username",
-		username).getSingleResult();
+        return (User) em.createQuery("select u from User u where u.username = :username").setParameter("username",
+                username).getSingleResult();
     }
 
 }
--- xebia-spring-travel-ecommerce/src/main/java/org/springframework/webflow/samples/util/JpaLogger.java
+++ xebia-spring-travel-ecommerce/src/main/java/org/springframework/webflow/samples/util/JpaLogger.java
@@ -1,79 +1,79 @@
 package org.springframework.webflow.samples.util;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.List;
-
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.List;
+
 @Aspect
 @SuppressWarnings("rawtypes")
 public class JpaLogger {
-	private final Logger LOGGER = LoggerFactory
-			.getLogger("org.springframework.webflow.samples.util.JpaLogger");
+    private final Logger LOGGER = LoggerFactory
+            .getLogger("org.springframework.webflow.samples.util.JpaLogger");
 
-	private boolean isEnabled = true;
+    private boolean isEnabled = true;
 
-	public void setEnabled(boolean isEnabled) {
-			this.isEnabled=isEnabled;
-	}
+    public void setEnabled(boolean isEnabled) {
+        this.isEnabled = isEnabled;
+    }
 
-	@AfterReturning(pointcut = "execution(* org.springframework.webflow.samples.booking.JpaBookingService.findHotels(..))", returning = "obj")
-	public void logReturnedObject(Object obj) {
-		if (isEnabled) {
-			if (obj instanceof List) {
-				List list = (List) obj;
-				for (Object resultObject : list) {
-					invokeGetters(resultObject);
-				}
-			} else {
-				invokeGetters(obj);
-			}
-		}
-	}
+    @AfterReturning(pointcut = "execution(* org.springframework.webflow.samples.booking.JpaBookingService.findHotels(..))", returning = "obj")
+    public void logReturnedObject(Object obj) {
+        if (isEnabled) {
+            if (obj instanceof List) {
+                List list = (List) obj;
+                for (Object resultObject : list) {
+                    invokeGetters(resultObject);
+                }
+            } else {
+                invokeGetters(obj);
+            }
+        }
+    }
 
-	private void invokeGetters(Object o) {
-		Method[] m = null;
-		Class c = null;
+    private void invokeGetters(Object o) {
+        Method[] m = null;
+        Class c = null;
 
-		c = o.getClass();
-		m = c.getMethods();
-		logMethods(m, o);
-	}
+        c = o.getClass();
+        m = c.getMethods();
+        logMethods(m, o);
+    }
 
-	private void logMethods(Method[] m, Object o) {
-		Class[] params = null;
-		for (int i = 0; i < m.length; ++i) {
-			StringBuffer buffer = new StringBuffer();
-			buffer.append(Modifier.toString(m[i].getModifiers()));
-			buffer.append(" ");
-			buffer.append(m[i].getReturnType().getName());
-			buffer.append(" ");
-			buffer.append(m[i].getName());
-			buffer.append("(");
-			params = m[i].getParameterTypes();
-			for (int j = 0; j < params.length; ++j) {
-				buffer.append(params[j].getName());
-			}
-			buffer.append(")");
-			if (m[i].getName().startsWith("get")) {
-				buffer.append(" - value =");
-				try {
-					buffer.append(m[i].invoke(o, new Object[] {}));
-				} catch (IllegalArgumentException e) {
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
-				} catch (InvocationTargetException e) {
-					e.printStackTrace();
-				}
-			}
-			LOGGER.warn(buffer.toString());
-		}
-	}
+    private void logMethods(Method[] m, Object o) {
+        Class[] params = null;
+        for (int i = 0; i < m.length; ++i) {
+            StringBuffer buffer = new StringBuffer();
+            buffer.append(Modifier.toString(m[i].getModifiers()));
+            buffer.append(" ");
+            buffer.append(m[i].getReturnType().getName());
+            buffer.append(" ");
+            buffer.append(m[i].getName());
+            buffer.append("(");
+            params = m[i].getParameterTypes();
+            for (int j = 0; j < params.length; ++j) {
+                buffer.append(params[j].getName());
+            }
+            buffer.append(")");
+            if (m[i].getName().startsWith("get")) {
+                buffer.append(" - value =");
+                try {
+                    buffer.append(m[i].invoke(o, new Object[]{}));
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+            LOGGER.warn(buffer.toString());
+        }
+    }
 
 }
