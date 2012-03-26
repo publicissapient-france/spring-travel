70f993bc057d767941806101923477d671bdd92b
Séven Le Mesle <slemesle@xebia.fr>
2012-03-27 00:00:21 +0200

    SLM - Append gc logs to a file

--- xebia-spring-travel-assembly/apache-tomcat/bin/setenv.sh
+++ xebia-spring-travel-assembly/apache-tomcat/bin/setenv.sh
@@ -1,2 +1,2 @@
-export JAVA_OPTS="-client -XX:+PrintGCDetails -XX:+UseSerialGC -Xmx512m -XX:MaxPermSize=128M"
+export JAVA_OPTS="-client -XX:+PrintGCDetails -Xloggc:$CATALINA_HOME/logs/GC.log -XX:+UseSerialGC -Xmx512m -XX:MaxPermSize=128M"
 export CATALINA_OPTS=" -Dcom.sun.management.jmxremote -Dcom.sun.jmxremote.port=8084 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
